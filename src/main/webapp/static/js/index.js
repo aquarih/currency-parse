c32cdcea6af51f6914c696ca34ddfc565e7798e6
aquarih <71595611+aquarih@users.noreply.github.com>
2022-06-14 01:25:54 +0800

    防呆驗證

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -14,52 +14,58 @@ $('.dropdown-item').click(function (){
         "timeout": 0,
     };
 
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        var jsonRes = JSON.parse(response);
-        var buy = jsonRes.Line.series[0].data;
-        var buyPriceArray = [];
-        var timeArray = [];
-        for(var i = 0;i < buy.length;i++){
-            var time = buy[i][0];
-            var price = buy[i][1];
-            timeArray.push(new Date(time).toLocaleDateString());
-            buyPriceArray.push(price);
-        }
+    $.ajax(settings).done(function (response, status, xhr){
+        if(xhr.status !== 200){
+            alert("error");
+        }else{
+            console.log(response);
+            var jsonRes = JSON.parse(response);
+            var buy = jsonRes.Line.series[0].data;
+            var buyPriceArray = [];
+            var timeArray = [];
+            for(var i = 0;i < buy.length;i++){
+                var time = buy[i][0];
+                var price = buy[i][1];
+                timeArray.push(new Date(time).toLocaleDateString());
+                buyPriceArray.push(price);
+            }
 
-        var sell = jsonRes.Line.series[1].data;
-        var sellPriceArray = [];
-        for(var i = 0;i < buy.length;i++){
-            var price = sell[i][1];
-            sellPriceArray.push(price);
-        }
+            var sell = jsonRes.Line.series[1].data;
+            var sellPriceArray = [];
+            for(var i = 0;i < buy.length;i++){
+                var price = sell[i][1];
+                sellPriceArray.push(price);
+            }
 
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: timeArray,
-                datasets: [
-                    {
-                        label: 'Buy',
-                        data: buyPriceArray,
-                        backgroundColor: "rgba(255,99,132,0.5)",
-                    },
-                    {
-                        label: 'Sell',
-                        data: sellPriceArray,
-                        backgroundColor: "rgba(0,255,132,0.5)",
-                    }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    y: {
-                        min: Math.min(sellPriceArray),
-                        max: Math.max(buyPriceArray),
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: timeArray,
+                    datasets: [
+                        {
+                            label: 'Buy',
+                            data: buyPriceArray,
+                            borderColor:"rgba(255,99,132,0.5)",
+                            backgroundColor: "rgba(255,99,132,0.2)",
+                        },
+                        {
+                            label: 'Sell',
+                            data: sellPriceArray,
+                            borderColor:"rgba(0,255,132,0.5)",
+                            backgroundColor: "rgba(0,255,132,0.2)",
+                        }]
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        y: {
+                            min: Math.min(sellPriceArray),
+                            max: Math.max(buyPriceArray),
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
     });
 })
